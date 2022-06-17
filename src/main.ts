3a4fbe2f160321046ca15a483fcb3c4cf28559e8
runlong-yao <50858336+runlong-yao@users.noreply.github.com>
2022-06-17 11:33:50 +0800

    Update main.ts
    remove  import router code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import router from "./router";
+
 import "./styles/tailwind.css";
 import "./styles/antd.less";
 
-createApp(App).use(router).mount("#app");
+createApp(App).mount("#app");
